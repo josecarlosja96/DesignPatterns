319b70b1946ea8c7e677e728c90c6b280cc18012
José Carlos <josecarlosja96@gmail.com>
2021-04-24 10:01:36 +0200

    [+] Añadidas dependencias mvn scalatest y creado primer test de prueba.

---
+++ AbstractFactory/src/test/java/AbstractFactoryTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class AbstractFactoryTest extends AnyFlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    assert(1===1)
+  }
+}
